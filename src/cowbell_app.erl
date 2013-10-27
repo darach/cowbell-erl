370af7fd489559aafb4dbc72b0e25b204a06018e
Paul Oliver <puzza007@gmail.com>
2013-10-27 18:05:02 +0100

    start_http params copied from the CloudI http listener

--- src/cowbell_app.erl
+++ src/cowbell_app.erl
@@ -21,7 +21,20 @@ start(_Type, _Args) ->
             {"/bench/throughput/10k", random_data_handler, [k10]},
             {"/bench/throughput/100k", random_data_handler, [k100]},
             {"/bench/throughput/1m", random_data_handler, [m1]} ]} ]),
-        {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [{env, [{dispatch, Dispatch}]}]),
+        {ok, _} = cowboy:start_http(http, 100, [{port, 8080},
+                                                {backlog, 128},
+                                                {nodelay, true},
+                                                {max_connections, 61000}],
+                                    [{env, [{dispatch, Dispatch},
+                                            {max_keepalive, 100},
+                                            {compress, false},
+                                            {max_empty_lines, 5},
+                                            {max_header_name_length, 64},
+                                            {max_header_value_length, 4096},
+                                            {max_headers, 100},
+                                            {max_keepalive, 100},
+                                            {max_request_line_length, 4096},
+                                            {timeout, 30000}]}]),
     cowbell_sup:start_link().
 
 stop(_State) ->
